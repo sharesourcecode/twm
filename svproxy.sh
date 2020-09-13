25b02c600d07aef868598438379f4b074d4bb28b
Ton Guitar <49400447+sharesourcecode@users.noreply.github.com>
2020-09-13 15:23:04 -0300

    Add files via upload

---
+++ svproxy.sh
@@ -0,0 +1,11 @@
+_proxy () {
+#Obtains proxy from the official server.
+	SPROXY=$(dig $URL | grep -oP '(\d+.\d+.\d+.\d+.\d+)' | sed 's/^.*$/&:80/' | head -n1)
+	PROXY="http://$SPROXY"
+	unset SPROXY
+	case $URL in
+		(tiwar.net)
+		URL='http://tiwar.net' ;;
+	esac
+#Use w3m -o http_proxy=$PROXY $URL...
+}
