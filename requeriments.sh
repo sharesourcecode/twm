646a9d8f50d67a27d1fb01bf23b956dbc7942a7e
Ton Guitar <49400447+sharesourcecode@users.noreply.github.com>
2020-09-14 10:59:32 -0300

    Add files via upload

--- requeriments.sh
+++ requeriments.sh
@@ -7,45 +7,47 @@ _requeriments () {
 	_sync () {
 		cd $HOME/twm
 		curl https://github.com/sharesourcecode/twm/raw/master/cave.sh -O -L &> /dev/null ;
-		echo -e "\n Upgrading 1/20"
+		echo -e "\n Upgrading 1/21"
 		curl https://github.com/sharesourcecode/twm/raw/master/PLAY.sh -O -L &> /dev/null ;
-		echo -e "\n Upgrading 2/20"
+		echo -e "\n Upgrading 2/21"
 		curl https://github.com/sharesourcecode/twm/raw/master/altars.sh -O -L &> /dev/null ;
-		echo -e "\n Upgrading 3/20"
+		echo -e "\n Upgrading 3/21"
 		curl https://github.com/sharesourcecode/twm/raw/master/arena.sh -O -L &> /dev/null ;
-		echo -e "\n Upgrading 4/20"
+		echo -e "\n Upgrading 4/21"
 		curl https://github.com/sharesourcecode/twm/raw/master/campaign.sh -O -L &> /dev/null ;
-		echo -e "\n Upgrading 5/20"
+		echo -e "\n Upgrading 5/21"
 		curl https://github.com/sharesourcecode/twm/raw/master/career.sh -O -L &> /dev/null ;
-		echo -e "\n Upgrading 6/20"
+		echo -e "\n Upgrading 6/21"
 		curl https://github.com/sharesourcecode/twm/raw/master/clancoliseum.sh -O -L &> /dev/null ;
-		echo -e "\n Upgrading 7/20"
+		echo -e "\n Upgrading 7/21"
 		curl https://github.com/sharesourcecode/twm/raw/master/clandungeon.sh -O -L &> /dev/null ;
-		echo -e "\n Upgrading 8/20"
+		echo -e "\n Upgrading 8/21"
 		curl https://github.com/sharesourcecode/twm/raw/master/clanfight.sh -O -L &> /dev/null ;
-		echo -e "\n Upgrading 9/20"
+		echo -e "\n Upgrading 9/21"
 		curl https://github.com/sharesourcecode/twm/raw/master/clanid.sh -O -L &> /dev/null ;
-		echo -e "\n Upgrading 10/20"
+		echo -e "\n Upgrading 10/21"
 		curl https://github.com/sharesourcecode/twm/raw/master/coliseum.sh -O -L &> /dev/null ;
-		echo -e "\n Upgrading 11/20"
+		echo -e "\n Upgrading 11/21"
 		curl https://github.com/sharesourcecode/twm/raw/master/crono.sh -O -L &> /dev/null ;
-		echo -e "\n Upgrading 12/20"
+		echo -e "\n Upgrading 12/21"
 		curl https://github.com/sharesourcecode/twm/raw/master/king.sh -O -L &> /dev/null ;
-		echo -e "\n Upgrading 13/20"
+		echo -e "\n Upgrading 13/21"
 		curl https://github.com/sharesourcecode/twm/raw/master/loginlogoff.sh -O -L &> /dev/null ;
-		echo -e "\n Upgrading 14/20"
+		echo -e "\n Upgrading 14/21"
 		curl https://github.com/sharesourcecode/twm/raw/master/play.sh -O -L &> /dev/null ;
-		echo -e "\n Upgrading 15/20"
+		echo -e "\n Upgrading 15/21"
 		curl https://github.com/sharesourcecode/twm/raw/master/requeriments.sh -O -L &> /dev/null ;
-		echo -e "\n Upgrading 16/20"
+		echo -e "\n Upgrading 16/21"
 		curl https://github.com/sharesourcecode/twm/raw/master/trade.sh -O -L &> /dev/null ;
-		echo -e "\n Upgrading 17/20"
+		echo -e "\n Upgrading 17/21"
 		curl https://github.com/sharesourcecode/twm/raw/master/undying.sh -O -L &> /dev/null ;
-		echo -e "\n Upgrading 18/20"
+		echo -e "\n Upgrading 18/21"
 		curl https://github.com/sharesourcecode/twm/raw/master/flagfight.sh -O -L &> /dev/null ;
-		echo -e "\n Upgrading 19/20"
+		echo -e "\n Upgrading 19/21"
 		curl https://github.com/sharesourcecode/twm/raw/master/allies.sh -O -L &> /dev/null ;
-		echo -e "\n Upgrading 20/20"
+		echo -e "\n Upgrading 20/21"
+		curl https://github.com/sharesourcecode/twm/raw/master/svproxy.sh -O -L &> /dev/null ;
+		echo -e "\n Upgrading 21/21"
 		dos2unix *.sh &> /dev/null
 	}
 # /termux on android
@@ -54,7 +56,7 @@ _requeriments () {
 	if [[ $? = 0 ]] ; then
 		[[ ! -e executed.txt ]] && pkg install termux-api w3m curl dos2unix -y && >executed.txt
 # _sync - to disable coment #
-#		[[ $(date +%H) -lt 10 || $(date +%H) -gt 22 ]] && _sync
+		[[ $(date +%H) -lt 10 || $(date +%H) -gt 22 ]] && _sync
 		reset; clear
 		echo -e "Successful updates!\n"
 	else
