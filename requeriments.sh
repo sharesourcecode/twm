176fd2d1f4d50221437e0629eeef5c936302c2af
Ton Guitar <49400447+sharesourcecode@users.noreply.github.com>
2020-09-16 23:58:21 -0300

    Add files via upload

--- requeriments.sh
+++ requeriments.sh
@@ -56,7 +56,7 @@ _requeriments () {
 	if [[ $? = 0 ]] ; then
 		[[ ! -e executed.txt ]] && pkg install termux-api w3m curl dos2unix -y && >executed.txt
 # _sync - to disable coment #
-#		[[ $(date +%H) -lt 10 || $(date +%H) -gt 22 ]] && _sync
+		[[ $(date +%H) -lt 10 || $(date +%H) -gt 22 ]] && _sync
 		reset; clear
 		echo -e "Successful updates!\n"
 	else
