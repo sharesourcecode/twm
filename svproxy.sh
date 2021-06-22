868ca35bb8f6c6f84004d59df3c628476ebdd44d
Ton Guitar <49400447+sharesourcecode@users.noreply.github.com>
2021-06-21 22:36:54 -0300

    Update svproxy.sh

--- svproxy.sh
+++ svproxy.sh
@@ -1,12 +1,12 @@
 _proxy () {
 #Obtains proxy from the official server.
-	SPROXY=$(dig $URL | grep -oP '(\d+.\d+.\d+.\d+.\d+)' | sed 's/^.*$/&:80/' | head -n1)
-	PROXY="http://$SPROXY"
+	SPROXY=$(dig $URL | grep -oP '(\d+.\d+.\d+.\d+.\d+)' | sed 's/^.*$/&:443/' | head -n1)
+	PROXY="https://$SPROXY"
 	unset SPROXY
 	echo "Server: $URL|$PROXY"
-	case $URL in
-		(tiwar.net)
-		URL='http://tiwar.net' ;;
-	esac
+#	case $URL in
+#		(tiwar.net)
+#		URL='http://tiwar.net' ;;
+#	esac
 #Use w3m -o http_proxy=$PROXY $URL...
 }
