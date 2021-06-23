6918904ee2b17c5bd617975d15a842dfd38c5e78
Ton Guitar <49400447+sharesourcecode@users.noreply.github.com>
2021-06-22 22:38:14 -0300

    Update easyinstall.sh

--- easyinstall.sh
+++ easyinstall.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 termux-wake-lock &> /dev/null
 #echo "deb https://dl.kcubeterm.me/termux-packages-24/ stable main" >$PREFIX/etc/apt/sources.list
-#echo "deb https://termux.mentality.rip/termux-games games stable" >$PREFIX/etc/apt/sources.list.d/game.list
+#echo "deb https://packages.termux.org/apt/termux-games games stable" >$PREFIX/etc/apt/sources.list.d/game.list
 #echo "deb https://termux.mentality.rip/termux-science science stable" >$PREFIX/etc/apt/sources.list.d/science.list
 [[ ! -e installed.txt ]] && pkg install coreutils ncurses-utils git termux-api w3m curl dos2unix dnsutils -y; >installed.txt
 rm -rf twm/ &> /dev/null
