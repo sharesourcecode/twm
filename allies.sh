20e7b1bdb6cf30f3dd64704a2b67c60315d7e547
Ton Guitar <49400447+sharesourcecode@users.noreply.github.com>
2021-06-24 22:55:21 -0300

    Add files via upload

--- allies.sh
+++ allies.sh
@@ -4,26 +4,36 @@ _members () {
 	_clanid
 	[[ -n $CLD ]] && {
 	echo -e "\nUpdating clan members into allies..."
-	for num in `seq 5 -1 1`; do $SOURCE "$URL/clan/$CLD/$num" -o user_agent="$(shuf -n1 .ua)" | grep -oP "/>\p{Lu}{1}\p{Ll}{0,15}[\ ]{0,1}\p{L}{0,14}, <s" | awk -F"[>]" '{print $2}' | awk -F"[,]" '{print $1}' | sed 's,\ ,_,' >>allies.txt; done
+	for num in `seq 5 -1 1`; do
+		$SOURCE "$URL/clan/$CLD/$num" -o user_agent="$(shuf -n1 .ua)" | grep -oP "/>\p{Lu}{1}\p{Ll}{0,15}[\ ]{0,1}\p{L}{0,14}, <s" | awk -F"[>]" '{print $2}' | awk -F"[,]" '{print $1}' | sed 's,\ ,_,' >>allies.txt &
+		x=$! ; sleep 5 && kill -9 $x &> /dev/null;
+	done
 	sort -u allies.txt -o allies.txt
 	}
 # Print info
 	echo -ne "\033[33m"; echo "Allies for Coliseum and King of the Immortals:"
 	cat allies.txt
 	echo -ne "\033[37m"
-	echo "Look UP↑ or wait to continue.  👈"
-	sleep 3
+	echo "Wait to continue.  👈"
+	sleep 5
 }
 _alliesID () {
 # Friends ID
 	cd $TMP
-	SRC=$($SOURCE "$URL/mail/friends" -o user_agent="$(shuf -n1 .ua)")
-	NPG=$(echo $SRC | sed 's/href=/\n/g' | grep "/mail/friends/[0-9]'>&#62;&#62;" | cut -d\' -f2 | cut -d\/ -f4)
+	echo $($SOURCE "$URL/mail/friends" -o user_agent="$(shuf -n1 .ua)") >SRC &
+	x=$! ; sleep 5 && kill -9 $x &> /dev/null;
+	NPG=$(cat SRC | sed 's/href=/\n/g' | grep "/mail/friends/[0-9]'>&#62;&#62;" | cut -d\' -f2 | cut -d\/ -f4)
 	>tmp.txt; echo -ne "\033[33m"
 	if [[ -z $NPG ]] ; then
-		$SOURCE "$URL/mail/friends" -o user_agent="$(shuf -n1 .ua)" | sed 's,/user/,\n/user/,g' |  grep "/user/" | grep "/mail/" | cut -d\< -f1 >>tmp.txt; echo "Looking for allies on friend list..."
+		$SOURCE "$URL/mail/friends" -o user_agent="$(shuf -n1 .ua)" | sed 's,/user/,\n/user/,g' |  grep "/user/" | grep "/mail/" | cut -d\< -f1 >>tmp.txt &
+		x=$! ; sleep 5 && kill -9 $x &> /dev/null;
+		echo "Looking for allies on friend list..."
 	else
-		for num in `seq $NPG -1 1`; do $SOURCE "$URL/mail/friends/$num" -o user_agent="$(shuf -n1 .ua)" | sed 's,/user/,\n/user/,g' | grep "/user/" | grep "/mail/" | cut -d\< -f1 >>tmp.txt; echo "Looking for allies on friend list page $num..."; done
+		for num in `seq $NPG -1 1`; do
+			$SOURCE "$URL/mail/friends/$num" -o user_agent="$(shuf -n1 .ua)" | sed 's,/user/,\n/user/,g' | grep "/user/" | grep "/mail/" | cut -d\< -f1 >>tmp.txt &
+			x=$! ; sleep 5 && kill -9 $x &> /dev/null;
+			echo "Looking for allies on friend list page $num...";
+		done
 	fi
 	sort -u tmp.txt -o tmp.txt
 	cat tmp.txt | cut -d\> -f2 | sed 's,\ ,_,' >allies.txt
@@ -39,9 +49,10 @@ _calliesID () {
 	echo "Clan allies by Leader/Deputy on friends list..."
 	while read IDN; do
 		if [[ -n $IDN ]]; then
-			SRC=$($SOURCE "$URL/user/$IDN" -o user_agent="$(shuf -n1 .ua)")
-			LEADPU=$(echo $SRC | sed 's,/clan/,\n/clan/,g' |  grep -E "</a>, <span class='blue'|</a>, <span class='green'" | cut -d\< -f1 |cut -d\> -f2)
-			alCLAN=$(echo $SRC | grep -E -o '/clan/[0-9]{1,3}' | tail -n1)
+			echo $($SOURCE "$URL/user/$IDN" -o user_agent="$(shuf -n1 .ua)") >SRC &
+			x=$! ; sleep 5 && kill -9 $x &> /dev/null;
+			LEADPU=$(cat SRC | sed 's,/clan/,\n/clan/,g' |  grep -E "</a>, <span class='blue'|</a>, <span class='green'" | cut -d\< -f1 |cut -d\> -f2)
+			alCLAN=$(cat SRC | grep -E -o '/clan/[0-9]{1,3}' | tail -n1)
 			[[ -n $LEADPU ]] && {
 			ts=$[$ts+1]
 			echo $LEADPU | sed 's,\ ,_,' >>callies.txt
@@ -55,7 +66,7 @@ _calliesID () {
 _alliesConf () {
 	cd $TMP
 	clear
-	echo -e "The script will consider users on your friends list and you Clan as allies, Leader/Deputy on friend list will add Clan allies.\n"
+	echo -e "The script will consider users on your friends list and Clan as allies, Leader/Deputy on friend list will add Clan allies.\n"
 	echo -e "\n1) Add/Update alliances(All Battles)\n\n2) Add/Update just Herois alliances(Coliseum/King of immortals)\n\n3) Add/Update just Clan alliances(Altars,Clan Coliseum and Clan Fight)\n\n4) Do nothing\n"
 	read -p "Set up alliances[1 to 4]: " -t 300 -e -n 1 AL
 	case $AL in
@@ -69,5 +80,5 @@ _alliesConf () {
 
 		(*) clear; [[ -n $AL ]] && echo -e "\n Invalid option: $(echo $AL)" && kill -9 $$ || echo -e "\n Time exceeded!" ;;
 	esac
-	unset ts IDN SRC LEADPU alCLA AL SRC NPG
+	unset ts IDN LEADPU alCLA AL NPG
 }
