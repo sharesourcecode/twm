69f2c7aeda2eae25fccc948092262acc44f83e9d
Ton Guitar <49400447+sharesourcecode@users.noreply.github.com>
2021-06-26 11:26:23 -0300

    Add files via upload

--- career.sh
+++ career.sh
@@ -1,32 +1,36 @@
 # /career
 # /career/attack/?r=8781779
 _career () {
-	echo "Checking career..."
+	echo "Career..."
 	_clanid
 	if [[ -n $CLD ]]; then
-		echo "Clan ID: $CLD"
-		$PAGE "$URL/clan/$CLD/quest/take/6" -o user_agent="$(shuf -n1 .ua)" | head -n4 | tail -n3 &
-		x=$! ; sleep 3 && kill -9 $x &> /dev/null
-		$PAGE "$URL/clan/$CLD/quest/help/6" -o user_agent="$(shuf -n1 .ua)" | head -n4 | tail -n3 &
-		x=$! ; sleep 3 && kill -9 $x &> /dev/null
+		$PAGE "$URL/clan/$CLD/quest/take/6" -o user_agent="$(shuf -n1 .ua)" | tail -n0 &
+		x1=$! &> /dev/null ; echo "$URL/clan/$CLD/quest/take/6" ; sleep 2
+		$PAGE "$URL/clan/$CLD/quest/help/6" -o user_agent="$(shuf -n1 .ua)" | tail -n0 &
+		x2=$! &> /dev/null ; echo "$URL/clan/$CLD/quest/help/6" ; sleep 3
+		kill -9 $x1 $x2 &> /dev/null
 	fi
+	echo "$URL/career"
 	echo $($SOURCE "$URL/career" -o user_agent="$(shuf -n1 .ua)") >SRC &
-	x=$! ; sleep 3 && kill -9 $x &> /dev/null
+	x=$! &> /dev/null ; sleep 3
 	ENTER=$(cat SRC | sed 's/href=/\n/g' | grep -o 'career/attack')
 	ACCESS=$(cat SRC | sed 's/href=/\n/g' | grep '/career/attack/' | head -n1 | awk -F\' '{ print $2 }')
+	kill -9 $x &> /dev/null
 	until [[ -z $ENTER ]]; do
+		echo " ⚔ $URL$ACCESS"
 		echo $($SOURCE "$URL$ACCESS" -o user_agent="$(shuf -n1 .ua)") >SRC &
-		x=$! ; sleep 3 && kill -9 $x &> /dev/null
+		x=$! &> /dev/null ; sleep 3
 		ENTER=$(cat SRC | sed 's/href=/\n/g' | grep -o 'career/attack')
 		ACCESS=$(cat SRC | sed 's/href=/\n/g' | grep '/career/attack/' | head -n1 | awk -F\' '{ print $2 }')
-		echo " ⚔ $ACCESS"
+		kill -9 $x &> /dev/null
 	done
 	if [[ -n $CLD ]]; then
-		$PAGE "$URL/clan/$CLD/quest/deleteHelp/6" -o user_agent="$(shuf -n1 .ua)" | head -n4 | tail -n3 &
-		x=$! ; sleep 3 && kill -9 $x &> /dev/null
-		$PAGE "$URL/clan/$CLD/quest/end/6" -o user_agent="$(shuf -n1 .ua)" | head -n4 | tail -n3 & 
-		x=$! ; sleep 3 && kill -9 $x &> /dev/null
+		$PAGE "$URL/clan/$CLD/quest/deleteHelp/6" -o user_agent="$(shuf -n1 .ua)" | tail -n0 &
+		x1=$! &> /dev/null ; echo "$URL/clan/$CLD/quest/deleteHelp/6" ; sleep 2
+		$PAGE "$URL/clan/$CLD/quest/end/6" -o user_agent="$(shuf -n1 .ua)" | tail -n0 &
+		x2=$! &> /dev/null ; echo "$URL/clan/$CLD/quest/end/6" ; sleep 3
+		kill -9 $x1 $x2 &> /dev/null
 	fi
 	unset CLD ENTER ACCESS
-	echo -e "career (✔)\n"
+	echo -e "career (✔)\n" ; sleep 5
 }
