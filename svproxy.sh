3e0e4afaf069a8f7450769c49e91a39f84b0f77d
Ton Guitar <49400447+sharesourcecode@users.noreply.github.com>
2021-06-28 20:06:50 -0300

    Add files via upload

--- svproxy.sh
+++ svproxy.sh
@@ -3,10 +3,7 @@ _proxy () {
 	SPROXY=$(dig $URL | grep -oP '(\d+.\d+.\d+.\d+.\d+)' | sed 's/^.*$/&:443/' | head -n1)
 	PROXY="https://$SPROXY"
 	unset SPROXY
-	echo "Server: $URL|$PROXY"
-#	case $URL in
-#		(tiwar.net)
-#		URL='http://tiwar.net' ;;
-#	esac
+	echo -e "\e[01;30m\e[01;07m Server: $URL|$PROXY \e[00m"
+	sleep 3
 #Use w3m -o http_proxy=$PROXY $URL...
 }
