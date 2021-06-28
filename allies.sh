72597cdd208a3d5adc6b9d1d4caad0b51f396633
Ton Guitar <49400447+sharesourcecode@users.noreply.github.com>
2021-06-27 22:16:21 -0300

    Add files via upload

--- allies.sh
+++ allies.sh
@@ -3,9 +3,9 @@ _members () {
 	echo "Ilililililililil" >>allies.txt
 	_clanid
 	[[ -n $CLD ]] && {
-	echo -e "\n________________________________________\nUpdating clan members into allies..."
+	echo -e "\e[01;30m\e[05;07m\nUpdating clan members into allies\n\e[00m"
 	for num in `seq 5 -1 1`; do
-		echo "/clan/$CLD/$num ..."
+		echo -e "\e[03;34m\e[02;04m/clan/$CLD/$num\e[00m"
 		$SOURCE "$URL/clan/$CLD/$num" -o user_agent="$(shuf -n1 .ua)" | grep -oP "/>\p{Lu}{1}\p{Ll}{0,15}[\ ]{0,1}\p{L}{0,14}, <s" | awk -F"[>]" '{print $2}' | awk -F"[,]" '{print $1}' | sed 's,\ ,_,' >>allies.txt &
 		sleep 2
 	done
@@ -14,28 +14,27 @@ _members () {
 	killall -q -9 w3m
 	}
 # Print info
-	echo -ne "\033[33m"; echo "Allies for Coliseum and King of the Immortals:"
+	echo -e "\e[01;30m\e[05;07m\nAllies for Coliseum and King of the Immortals:\n\e[00m"
 	cat allies.txt
-	echo -ne "\033[37m"
-	echo "Wait to continue.  👈"
+	echo -e "\e[01;30m\e[05;07mWait to continue.  👈\e[00m"
 	sleep 5
 }
 _alliesID () {
 # Friends ID
-	echo -e "\n________________________________________\nLooking for allies on friends list..."
+	echo -e "\e[01;30m\e[05;07mLooking for allies on friends list\e[00m"
 	cd $TMP
-	echo "/mail/friends ..."
+	echo -e "\e[03;34m\e[02;04m/mail/friends\e[00m"
 	echo $($SOURCE "$URL/mail/friends" -o user_agent="$(shuf -n1 .ua)") >SRC &
 	sleep 2
 	NPG=$(cat SRC | sed 's/href=/\n/g' | grep "/mail/friends/[0-9]'>&#62;&#62;" | cut -d\' -f2 | cut -d\/ -f4)
-	>tmp.txt; echo -ne "\033[33m"
+	>tmp.txt
 	if [[ -z $NPG ]] ; then
-		echo "/mail/friends ..."
+		echo -e "\e[03;34m\e[02;04m/mail/friends\e[00m"
 		$SOURCE "$URL/mail/friends" -o user_agent="$(shuf -n1 .ua)" | sed 's,/user/,\n/user/,g' |  grep "/user/" | grep "/mail/" | cut -d\< -f1 >>tmp.txt &
 		sleep 2
 	else
 		for num in `seq $NPG -1 1`; do
-			echo -e "Friends list page ...\n/mail/friends/$num";
+			echo -e "\e[01;30m\e[05;07mFriends list page $num\e[00m\n\e[03;34m\e[02;04m/mail/friends/$num\e[00m";
 			$SOURCE "$URL/mail/friends/$num" -o user_agent="$(shuf -n1 .ua)" | sed 's,/user/,\n/user/,g' | grep "/user/" | grep "/mail/" | cut -d\< -f1 >>tmp.txt &
 			sleep 2
 		done
@@ -51,21 +50,21 @@ _calliesID () {
 	[[ -n $CLD ]] && {
 	cd $TMP
 	ts=0
-	echo "Ilililililililililil" >callies.txt; echo -ne "\033[36m"
+	echo "Ilililililililililil" >callies.txt
 	cat tmp.txt | cut -d/ -f3 >ids.txt
-	echo -e "\n________________________________________\nClan allies by Leader/Deputy on friends list..."
+	echo -e "\e[01;30m\e[05;07m\nClan allies by Leader/Deputy on friends list\n\e[00m"
 	while read IDN; do
 		if [[ -n $IDN ]]; then
-			echo "/user/$IDN ..."
+			echo -e "\e[03;34m\e[02;04m/user/$IDN\e[00m"
 			echo $($SOURCE "$URL/user/$IDN" -o user_agent="$(shuf -n1 .ua)") >SRC &
 			sleep 2
 			LEADPU=$(cat SRC | sed 's,/clan/,\n/clan/,g' |  grep -E "</a>, <span class='blue'|</a>, <span class='green'" | cut -d\< -f1 |cut -d\> -f2)
 			alCLAN=$(cat SRC | grep -E -o '/clan/[0-9]{1,3}' | tail -n1)
-			echo "$LEADPU - $alCLAN"
+			echo -e "\e[03;34m\e[02;03m $LEADPU - $alCLAN\e[00m"
 			[[ -n $LEADPU ]] && {
 			ts=$[$ts+1]
 			echo $LEADPU | sed 's,\ ,_,' >>callies.txt
-			echo -e "$ts. Ally $LEADPU $alCLAN added.\n_ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _"
+			echo -e "\e[01;30m\e[05;07m $ts. Ally $LEADPU $alCLAN added.\e[00m"
 			sort -u callies.txt -o callies.txt
 			}
 		fi
@@ -77,8 +76,7 @@ _calliesID () {
 _alliesConf () {
 	cd $TMP
 	clear
-	echo -e "The script will consider users on your friends list and Clan as allies, Leader/Deputy on friend list will add Clan allies.\n"
-	echo -e "\n1) Add/Update alliances(All Battles)\n\n2) Add/Update just Herois alliances(Coliseum/King of immortals)\n\n3) Add/Update just Clan alliances(Altars,Clan Coliseum and Clan Fight)\n\n4) Do nothing\n"
+	echo -e "\e[01;30m\e[05;07m\nThe script will consider users on your friends list and \nClan as allies.\nLeader/Deputy on friend list will add \nClan allies.\n\e[00m\n1) Add/Update alliances(All Battles)\n\n2) Add/Update just Herois alliances(Coliseum/King of immortals)\n\n3) Add/Update just Clan alliances(Altars,Clan Coliseum and Clan Fight)\n\n4) Do nothing\n"
 	read -p "Set up alliances[1 to 4]: " -t 300 -e -n 1 AL
 	case $AL in
 		(1) _alliesID; _calliesID; _members; ALD=1; echo "Alliances on all battles active" ;;
