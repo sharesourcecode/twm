e554e5ae464abff607a4ad88d1d8194e8b882e9a
Ton Guitar <49400447+sharesourcecode@users.noreply.github.com>
2021-06-28 21:54:52 -0300

    Add files via upload

--- trade.sh
+++ trade.sh
@@ -1,47 +1,52 @@
 # /trade
 _trade () {
-	echo "Checking for gold exchange..."
+	echo "trade"
 	echo $($SOURCE "$URL/trade/exchange" -o user_agent="$(shuf -n1 .ua)") >SRC &
-	x=$! ; sleep 5 && kill -9 $x &> /dev/null
+	sleep 5
 	ACCESS=$(cat SRC | sed "s/\/trade/\\n/g" | grep 'exchange'| grep 'silver/1' | head -n1 | awk -F\= '{ print $2 }' | awk -F\' '{ print $1 }')
 	EXIST=$(cat SRC | sed "s/\/trade/\\n/g" | grep 'exchange' | grep 'silver/1' | head -n1 | awk -F\/ '{ print $3 }')
 	while [[ -n $EXIST ]]; do
+		echo -e "/trade/exchange/silver/1?r=$ACCESS"
 		echo $($SOURCE "$URL/trade/exchange/silver/1?r=$ACCESS" -o user_agent="$(shuf -n1 .ua)") >SRC &
-		x=$! ; sleep 3 && kill -9 $x &> /dev/null
+		sleep 3
 		ACCESS=$(cat SRC | sed "s/\/trade/\\n/g" | grep 'exchange'| grep 'silver/1' | head -n1 | awk -F\= '{ print $2 }' | awk -F\' '{ print $1 }')
 		EXIST=$(cat SRC | sed "s/\/trade/\\n/g" | grep 'exchange' | grep 'silver/1' | head -n1 | awk -F\/ '{ print $3 }')
-		echo -e "/trade/exchange/silver/1?r=$ACCESS"
+		killall -q -9 w3m
 	done
 	unset ACCESS EXIST
-	echo -e "Exchange (✔)\n"
+	echo -e "trade (✔)\n"
 }
 _money () {
 	if [[ -n $CLD ]] ; then
+		echo -e "clan money"
 		echo $($SOURCE $URL/arena/quit -o user_agent="$(shuf -n1 .ua)" | sed "s/href='/\n/g" | grep "attack/1" | head -n1 | awk -F\/ '{ print $5 }' | tr -cd "[[:digit:]]") >CODE &
-		x=$! ; sleep 3 && kill -9 $x &> /dev/null
+		sleep 3
 		echo -e "/clan/$CLD/money/?r=`cat CODE`&silver=1000&gold=2&confirm=true&type=limit"
-		$DUMP "$URL/clan/$CLD/money/?r=`cat CODE`&silver=1000&gold=1&confirm=true&type=limit" -o user_agent="$(shuf -n1 .ua)" &
-		x=$! ; sleep 3 && kill -9 $x &> /dev/null
+		$PAGE "$URL/clan/$CLD/money/?r=`cat CODE`&silver=1000&gold=1&confirm=true&type=limit" -o user_agent="$(shuf -n1 .ua)" | tail -n 0 &
+		sleep 3
 		echo $($SOURCE $URL/arena/quit -o user_agent="$(shuf -n1 .ua)" | sed "s/href='/\n/g" | grep "attack/1" | head -n1 | awk -F\/ '{ print $5 }' | tr -cd "[[:digit:]]") >CODE &
-		x=$! ; sleep 3 && kill -9 $x &> /dev/null
+		sleep 4
 		echo -e "/clan/$CLD/money/?r=`cat CODE`&silver=0&gold=1&confirm=true&type=limit"
-		$DUMP "$URL/clan/$CLD/money/?r=`cat CODE`&silver=1000&gold=1&confirm=true&type=limit" -o user_agent="$(shuf -n1 .ua)" &
-		x=$! ; sleep 3 && kill -9 $x &> /dev/null
-		echo -e "Clan Money (✔)\n"
+		$PAGE "$URL/clan/$CLD/money/?r=`cat CODE`&silver=1000&gold=1&confirm=true&type=limit" -o user_agent="$(shuf -n1 .ua)" | tail -n 0 &
+		sleep 4
+		echo -e "clan money (✔)\n"
+		killall -q -9 w3m
 	fi
 }
 _built () {
 	if [[ -n $CLD ]] ; then
+		echo -e "clan built upgrade"
 		echo $($SOURCE $URL/arena/quit -o user_agent="$(shuf -n1 .ua)" | sed "s/href='/\n/g" | grep "attack/1" | head -n1 | awk -F\/ '{ print $5 }' | tr -cd "[[:digit:]]") >CODE &
-		x=$! ; sleep 3 && kill -9 $x &> /dev/null
+		sleep 3
 		echo -e "/clan/$CLD/built/?goldUpgrade=true&r=`cat CODE`"
-		$DUMP "$URL/clan/$CLD/built/?goldUpgrade=true&r=`cat CODE`" -o user_agent="$(shuf -n1 .ua)" &
-		x=$! ; sleep 3 && kill -9 $x &> /dev/null
+		$PAGE "$URL/clan/$CLD/built/?goldUpgrade=true&r=`cat CODE`" -o user_agent="$(shuf -n1 .ua)" | tail -n 0 &
+		sleep 3
 		echo $($SOURCE $URL/arena/quit -o user_agent="$(shuf -n1 .ua)" | sed "s/href='/\n/g" | grep "attack/1" | head -n1 | awk -F\/ '{ print $5 }' | tr -cd "[[:digit:]]") >CODE &
-		x=$! ; sleep 3 && kill -9 $x &> /dev/null
+		sleep 3
 		echo -e "/clan/$CLD/built/?silverUpgrade=true&r=`cat CODE`"
-		$DUMP "$URL/clan/$CLD/built/?silverUpgrade=true&r=`cat CODE`" -o user_agent="$(shuf -n1 .ua)" &
-		x=$! ; sleep 3 && kill -9 $x &> /dev/null
-		echo -e "Clan Built upgrade (✔)\n"
+		$PAGE "$URL/clan/$CLD/built/?silverUpgrade=true&r=`cat CODE`" -o user_agent="$(shuf -n1 .ua)" | tail -n 0 &
+		sleep 3
+		echo -e "clan built upgrade (✔)\n"
+		killall -q -9 w3m
 	fi
 }
