53301f9d4087ed9639bc15f021458be0b001b905
Ton Guitar <49400447+sharesourcecode@users.noreply.github.com>
2021-06-29 08:51:11 -0300

    Add files via upload

--- play.sh
+++ play.sh
@@ -1,24 +1,14 @@
 _play () {
 	_all () {
-#		_AtakeHelp
-		echo Arena & _arena
-		sleep 3
-		echo "Open Chest..." & _openChest
-		sleep 3
-#		_AdeleteEnd
-		echo "Cave..." & _cave
-		sleep 3
-		echo "Campaign..." & _campaign
-		sleep 3
-		echo "Career..." & _career
-		sleep 3
-		echo "Clan Dungeon..." & _clandungeon
-		sleep 3
-		echo "Trade..." &_trade
-		sleep 3
-		echo "Clan Money..." & _money
-		sleep 3
-#		_built
+		_arena
+		_openChest
+		_cave
+		_campaign
+		_career
+		_clandungeon
+		_trade
+		_money
+		_built
 		echo "" & _msgs
 		sleep 5
 	}
@@ -38,24 +28,26 @@ _play () {
 				[[ $(date +%M) > 00 ]] && break
 			done
 			echo $($SOURCE "$URL/undying/enterGame" -o user_agent="$(shuf -n1 .ua)") >SRC &
-			x=$! ; sleep 5 && kill -9 $x &> /dev/null
-			echo "Undying..." & _undying
+			sleep 5
+			_undying
 			sleep 30
 			_crono ;;
+
 # /Battle of banners 10:15:00 - 16:15:00
 		(10:14|16:14)
 #			_msgs
 			echo $($SOURCE "$URL/flagfight/enterFight" -o user_agent="$(shuf -n1 .ua)") >SRC &
-			x=$! ; sleep 5 && kill -9 $x &> /dev/null
+			sleep 5
 			until [[ $(date +%M:%S) = 14:5* ]] ; do
 				echo 'Battle of banners will be started...'
 				sleep 1
 				[[ $(date +%M) > 15 ]] && break
 			done
 			echo $($SOURCE "$URL/flagfight/enterFight" -o user_agent="$(shuf -n1 .ua)") >SRC &
-			x=$! ; sleep 5 && kill -9 $x &> /dev/null
+			sleep 5
 			_flagfight
 			_crono ;;
+
 # /Clan coliseum 10:30:00 - 15:00:00
 		(10:29|14:59)
 			echo "" & _msgs
@@ -66,26 +58,28 @@ _play () {
 				[[ $(date +%M) = 00 ]] && break
 			done
 			echo $($SOURCE "$URL/clancoliseum/?close=reward" -o user_agent="$(shuf -n1 .ua)") >SRC &
-			x=$! ; sleep 5 && kill -9 $x &> /dev/null
+			sleep 5
 			echo $($SOURCE "$URL/clancoliseum/enterFight" -o user_agent="$(shuf -n1 .ua)") >SRC &
-			x=$! ; sleep 5 && kill -9 $x &> /dev/null
-			echo "Clan Coliseum..." & _clancoliseum
+			sleep 5
+			_clancoliseum
 			sleep 30
 			_crono ;;
+
 # /Clan tournament 11:00:00 - 19:00:00
 		(10:59|18:59)
 			echo "" & _msgs
 			sleep 3
 			echo $($SOURCE "$URL/clanfight/enterFight" -o user_agent="$(shuf -n1 .ua)") >SRC &
-			x=$! ; sleep 5 && kill -9 $x &> /dev/null
+			sleep 5
 			until [[ $(date +%M:%S) = 59:40 ]] ; do
 				echo 'Clan tournament will be started...'
 				sleep 1
 				[[ $(date +%M) = 00 ]] && break
 			done
-			echo "Clan Fight..." & _clanfight
+			_clanfight
 			sleep 30
 			_crono ;;
+
 # /King of the Immortals 12:30:00 - 16:30:00 - 22:30:00
 		(12:29|16:29|22:29)
 			echo "" & _msgs
@@ -96,12 +90,13 @@ _play () {
 				[[ $(date +%M) > 30 ]] && break
 			done
 			echo $($SOURCE "$URL/king/enterGame" -o user_agent="$(shuf -n1 .ua)") >SRC &
-			x=$! ; sleep 5 && kill -9 $x &> /dev/null
-			echo "King..." & _king
+			sleep 5
+			_king
 			sleep 30
-			echo "Arena..." & _arena
+			_arena
 			sleep 30
 			_crono ;;
+
 # /Ancient Altars 14:00:00 - 21:00:00
 		(13:59|20:59)
 			echo "" & _msgs
@@ -117,16 +112,18 @@ _play () {
 				[[ $(date +%M) = 00 ]] && break
 			done
 			echo $($SOURCE "$URL/altars/enterFight" -o user_agent="$(shuf -n1 .ua)") >SRC &
-			echo "Altars..." & _altars
+			_altars
 			sleep 30
 			_crono ;;
+
 		(0[0123456789]:[01234]$L|1[0248]:[01234]$L|20:[01234]$L|1[13579]:[234]$L|2[13]:[234]$L)
 			echo "" & _msgs
 			sleep 3
 			_all ;
-			echo "Coliseum..." & _coliseum
+			_coliseum
 			sleep 30
 			_crono ;;
+
 		(*)
 			_sleep ;
 			_crono ;;
