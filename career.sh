41df1d3cbac65a8d4d18c0dbf9a74ea971c91a15
Ton Guitar <49400447+sharesourcecode@users.noreply.github.com>
2021-07-04 08:57:05 -0300

    Add files via upload

--- career.sh
+++ career.sh
@@ -5,31 +5,30 @@ _career () {
 	_clanid
 	if [[ -n $CLD ]]; then
 		$PAGE "$URL/clan/$CLD/quest/take/6" -o user_agent="$(shuf -n1 .ua)" | tail -n0 &
-		x1=$! &> /dev/null ; echo "$URL/clan/$CLD/quest/take/6" ; sleep 2
+		echo "/clan/$CLD/quest/take/6" ; sleep 2
 		$PAGE "$URL/clan/$CLD/quest/help/6" -o user_agent="$(shuf -n1 .ua)" | tail -n0 &
-		x2=$! &> /dev/null ; echo "$URL/clan/$CLD/quest/help/6" ; sleep 3
-		kill -9 $x1 $x2 &> /dev/null
+		echo "/clan/$CLD/quest/help/6" ; sleep 3
+		killall -q -9 w3m
 	fi
-	echo "$URL/career"
+	echo "/career"
 	echo $($SOURCE "$URL/career" -o user_agent="$(shuf -n1 .ua)") >SRC &
-	x=$! &> /dev/null ; sleep 3
+	sleep 3
 	ENTER=$(cat SRC | sed 's/href=/\n/g' | grep -o 'career/attack')
 	ACCESS=$(cat SRC | sed 's/href=/\n/g' | grep '/career/attack/' | head -n1 | awk -F\' '{ print $2 }')
-	kill -9 $x &> /dev/null
 	until [[ -z $ENTER ]]; do
-		echo " ⚔ $URL$ACCESS"
-		echo $($SOURCE "$URL$ACCESS" -o user_agent="$(shuf -n1 .ua)") >SRC &
-		x=$! &> /dev/null ; sleep 3
+		echo " ⚔ $ACCESS"
+		echo $($SOURCE "$ACCESS" -o user_agent="$(shuf -n1 .ua)") >SRC &
+		sleep 3
 		ENTER=$(cat SRC | sed 's/href=/\n/g' | grep -o 'career/attack')
 		ACCESS=$(cat SRC | sed 's/href=/\n/g' | grep '/career/attack/' | head -n1 | awk -F\' '{ print $2 }')
-		kill -9 $x &> /dev/null
+		killall -q -9 w3m
 	done
 	if [[ -n $CLD ]]; then
 		$PAGE "$URL/clan/$CLD/quest/deleteHelp/6" -o user_agent="$(shuf -n1 .ua)" | tail -n0 &
-		x1=$! &> /dev/null ; echo "$URL/clan/$CLD/quest/deleteHelp/6" ; sleep 2
+		echo "/clan/$CLD/quest/deleteHelp/6" ; sleep 2
 		$PAGE "$URL/clan/$CLD/quest/end/6" -o user_agent="$(shuf -n1 .ua)" | tail -n0 &
-		x2=$! &> /dev/null ; echo "$URL/clan/$CLD/quest/end/6" ; sleep 3
-		kill -9 $x1 $x2 &> /dev/null
+		echo "/clan/$CLD/quest/end/6" ; sleep 3
+		killall -q -9 w3m
 	fi
 	unset CLD ENTER ACCESS
 	echo -e "career (✔)\n" ; sleep 5
