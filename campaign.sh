da5bf7f8318878012e1b309f7c64ad70cdde677c
Ton Guitar <49400447+sharesourcecode@users.noreply.github.com>
2021-07-07 15:15:42 -0300

    Add files via upload

--- campaign.sh
+++ campaign.sh
@@ -1,15 +1,18 @@
 # /campaign
 _campaign () {
+	echo "campaign ..."
 	echo $($SOURCE "$URL/campaign" -o user_agent="$(shuf -n1 .ua)") >SRC &
-	x=$! ; sleep 5 && kill -9 $x &> /dev/null
+	sleep 5
 	ENTER=$(cat SRC | sed "s/href='/\n/g" | grep "/campaign/" | head -n1 | awk -F\' '{ print $1 }' | awk -F\/ '{ print $3 }')
 	ACCESS=$(cat SRC | sed "s/href='/\n/g" | grep "/campaign/" | head -n1 | awk -F\' '{ print $1 }')
 	while [[ -n $ENTER && -n $ACCESS ]]; do
 		echo $($SOURCE $URL$ACCESS -o user_agent="$(shuf -n1 .ua)") >SRC &
-		x=$! ; sleep 3 && kill -9 $x &> /dev/null
+		sleep 3
 		ENTER=$(cat SRC | sed "s/href='/\n/g" | grep "/campaign/" | head -n1 | awk -F\' '{ print $1 }' | awk -F\/ '{ print $3 }')
 		ACCESS=$(cat SRC | sed "s/href='/\n/g" | grep "/campaign/" | head -n1 | awk -F\' '{ print $1 }')
 		echo "$ACCESS"
+		killall -q -9 w3m
 	done
 	unset ENTER ACCESS
+	echo -e "campaign (✔)\n"
 }
--- trade.sh
+++ trade.sh
@@ -1,6 +1,6 @@
 # /trade
 _trade () {
-	echo "trade"
+	echo "trade ..."
 	echo $($SOURCE "$URL/trade/exchange" -o user_agent="$(shuf -n1 .ua)") >SRC &
 	sleep 5
 	ACCESS=$(cat SRC | sed "s/\/trade/\\n/g" | grep 'exchange'| grep 'silver/1' | head -n1 | awk -F\= '{ print $2 }' | awk -F\' '{ print $1 }')
@@ -18,7 +18,7 @@ _trade () {
 }
 _money () {
 	if [[ -n $CLD ]] ; then
-		echo -e "clan money"
+		echo -e "clan money ..."
 		echo $($SOURCE $URL/arena/quit -o user_agent="$(shuf -n1 .ua)" | sed "s/href='/\n/g" | grep "attack/1" | head -n1 | awk -F\/ '{ print $5 }' | tr -cd "[[:digit:]]") >CODE &
 		sleep 3
 		echo -e "/clan/$CLD/money/?r=`cat CODE`&silver=1000&gold=2&confirm=true&type=limit"
@@ -35,7 +35,7 @@ _money () {
 }
 _built () {
 	if [[ -n $CLD ]] ; then
-		echo -e "clan built upgrade"
+		echo -e "clan built ..."
 		echo $($SOURCE $URL/arena/quit -o user_agent="$(shuf -n1 .ua)" | sed "s/href='/\n/g" | grep "attack/1" | head -n1 | awk -F\/ '{ print $5 }' | tr -cd "[[:digit:]]") >CODE &
 		sleep 3
 		echo -e "/clan/$CLD/built/?goldUpgrade=true&r=`cat CODE`"
@@ -46,7 +46,7 @@ _built () {
 		echo -e "/clan/$CLD/built/?silverUpgrade=true&r=`cat CODE`"
 		$PAGE "$URL/clan/$CLD/built/?silverUpgrade=true&r=`cat CODE`" -o user_agent="$(shuf -n1 .ua)" | tail -n 0 &
 		sleep 3
-		echo -e "clan built upgrade (✔)\n"
+		echo -e "clan built (✔)\n"
 		killall -q -9 w3m
 	fi
 }
