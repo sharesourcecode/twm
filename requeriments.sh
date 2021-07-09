8c5ce68d4e0787b75c07c40c42c4fe214f5dbd58
Ton Guitar <49400447+sharesourcecode@users.noreply.github.com>
2021-07-09 13:58:15 -0300

    Add files via upload

--- requeriments.sh
+++ requeriments.sh
@@ -53,59 +53,11 @@ _requeriments () {
 	[[ -z $URL ]] && exit
 # /tmp dir
 	mkdir -p $TMP
-# /update script and dependencies
-	echo -e "\e[01;30m\e[01;07m\n Upgrading...\n👉 Please wait...☕👴\e[00m"
-	_sync () {
-		cd $HOME/twm
-		echo -e "\n Downloading 1/21 PLAY.sh"
-		curl https://raw.githubusercontent.com/sharesourcecode/twm/master/PLAY.sh -O -L &> /dev/null ;
-		echo -e "\n Downloading 2/21 allies.sh"
-		curl https://raw.githubusercontent.com/sharesourcecode/twm/master/allies.sh -O -L &> /dev/null ;
-		echo -e "\n Downloading 3/21 altars.sh"
-		curl https://raw.githubusercontent.com/sharesourcecode/twm/master/altars.sh -O -L &> /dev/null ;
-		echo -e "\n Downloading 4/21 arena.sh"
-		curl https://raw.githubusercontent.com/sharesourcecode/twm/master/arena.sh -O -L &> /dev/null ;
-		echo -e "\n Downloading 5/21 campaign.sh"
-		curl https://raw.githubusercontent.com/sharesourcecode/twm/master/campaign.sh -O -L &> /dev/null ;
-		echo -e "\n Downloading 6/21 career.sh"
-		curl https://raw.githubusercontent.com/sharesourcecode/twm/master/career.sh -O -L &> /dev/null ;
-		echo -e "\n Downloading 7/21 cave.sh"
-		curl https://raw.githubusercontent.com/sharesourcecode/twm/master/cave.sh -O -L &> /dev/null ;
-		echo -e "\n Downloading 8/21 clancoliseum.sh"
-		curl https://raw.githubusercontent.com/sharesourcecode/twm/master/clancoliseum.sh -O -L &> /dev/null ;
-		echo -e "\n Downloading 9/21 clandungeon.sh"
-		curl https://raw.githubusercontent.com/sharesourcecode/twm/master/clandungeon.sh -O -L &> /dev/null ;
-		echo -e "\n Downloading 10/21 clanfight.sh"
-		curl https://raw.githubusercontent.com/sharesourcecode/twm/master/clanfight.sh -O -L &> /dev/null ;
-		echo -e "\n Downloading 11/21 clanid.sh"
-		curl https://raw.githubusercontent.com/sharesourcecode/twm/master/clanid.sh -O -L &> /dev/null ;
-		echo -e "\n Downloading 12/21 coliseum.sh"
-		curl https://raw.githubusercontent.com/sharesourcecode/twm/master/coliseum.sh -O -L &> /dev/null ;
-		echo -e "\n Downloading 13/21 crono.sh"
-		curl https://raw.githubusercontent.com/sharesourcecode/twm/master/crono.sh -O -L &> /dev/null ;
-		echo -e "\n Downloading 14/21 flagfight.sh"
-		curl https://raw.githubusercontent.com/sharesourcecode/twm/master/flagfight.sh -O -L &> /dev/null ;
-		echo -e "\n Downloading 15/21 king.sh"
-		curl https://raw.githubusercontent.com/sharesourcecode/twm/master/king.sh -O -L &> /dev/null ;
-		echo -e "\n Downloading 16/21 loginlogoff.sh"
-		curl https://raw.githubusercontent.com/sharesourcecode/twm/master/loginlogoff.sh -O -L &> /dev/null ;
-		echo -e "\n Downloading 17/21 play.sh"
-		curl https://raw.githubusercontent.com/sharesourcecode/twm/master/play.sh -O -L &> /dev/null ;
-		echo -e "\n Downloading 18/21 requeriments.sh"
-		curl https://raw.githubusercontent.com/sharesourcecode/twm/master/requeriments.sh -O -L &> /dev/null ;
-		echo -e "\n Downloading 19/21 svproxy.sh"
-		curl https://raw.githubusercontent.com/sharesourcecode/twm/master/svproxy.sh -O -L &> /dev/null ;
-		echo -e "\n Downloading 20/21 trade.sh"
-		curl https://raw.githubusercontent.com/sharesourcecode/twm/master/trade.sh -O -L &> /dev/null ;
-		echo -e "\n Downloading 21/21 undying.sh"
-		curl https://raw.githubusercontent.com/sharesourcecode/twm/master/undying.sh -O -L &> /dev/null ;
-		dos2unix *.sh &> /dev/null
-	}
 # /termux on android
 	cd $TMP
 	termux-wake-lock &> /dev/null
 	[[ $? = 0 ]] && {
-		[[ ! -e executed.txt ]] && pkg install termux-api w3m curl dos2unix dnsutils -y && >executed.txt
+		[[ ! -e executed.txt ]] && pkg install termux-api w3m dos2unix dnsutils -y && >executed.txt
 # _sync - to disable coment #
 		[[ $(date +%H) < 10 || $(date +%H) > 22 ]] && _sync
 		mkdir -p ~/.termux/boot
@@ -114,7 +66,7 @@ _requeriments () {
 		reset; clear
 		echo -e "Successful updates!\n"
 	} || {
-		sudo apt install w3m curl dos2unix dnsutils -y
+		sudo apt install w3m dos2unix dnsutils -y
 		[[ $(date +%H) -lt 10 || $(date +%H) -gt 22 ]] && _sync
 		reset; clear
 	}
