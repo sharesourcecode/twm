45a0e9bec087e5646e0ff8bdf61d7e46e9817f52
Ton Guitar <49400447+sharesourcecode@users.noreply.github.com>
2021-07-13 10:00:36 -0300

    Add files via upload

--- easyinstall.sh
+++ easyinstall.sh
@@ -1,47 +1,48 @@
 _sync () {
+	SERVER="https://raw.githubusercontent.com/sharesourcecode/twm/master"
 	echo -e "\e[01;30m\e[01;07m\n Upgrading...\n👉 Please wait...☕👴\e[00m"
 	echo -e "\n Downloading 1/21 PLAY.sh"
-	curl https://raw.githubusercontent.com/sharesourcecode/twm/master/PLAY.sh -O -L &> /dev/null ;
+	curl $SERVER/PLAY.sh -O -L &> /dev/null ;
 	echo -e "\n Downloading 2/21 allies.sh"
-	curl https://raw.githubusercontent.com/sharesourcecode/twm/master/allies.sh -O -L &> /dev/null ;
+	curl $SERVER/allies.sh -O -L &> /dev/null ;
 	echo -e "\n Downloading 3/21 altars.sh"
-	curl https://raw.githubusercontent.com/sharesourcecode/twm/master/altars.sh -O -L &> /dev/null ;
+	curl $SERVER/altars.sh -O -L &> /dev/null ;
 	echo -e "\n Downloading 4/21 arena.sh"
-	curl https://raw.githubusercontent.com/sharesourcecode/twm/master/arena.sh -O -L &> /dev/null ;
+	curl $SERVER/arena.sh -O -L &> /dev/null ;
 	echo -e "\n Downloading 5/21 campaign.sh"
-	curl https://raw.githubusercontent.com/sharesourcecode/twm/master/campaign.sh -O -L &> /dev/null ;
+	curl $SERVER/campaign.sh -O -L &> /dev/null ;
 	echo -e "\n Downloading 6/21 career.sh"
-	curl https://raw.githubusercontent.com/sharesourcecode/twm/master/career.sh -O -L &> /dev/null ;
+	curl $SERVER/career.sh -O -L &> /dev/null ;
 	echo -e "\n Downloading 7/21 cave.sh"
-	curl https://raw.githubusercontent.com/sharesourcecode/twm/master/cave.sh -O -L &> /dev/null ;
+	curl $SERVER/cave.sh -O -L &> /dev/null ;
 	echo -e "\n Downloading 8/21 clancoliseum.sh"
-	curl https://raw.githubusercontent.com/sharesourcecode/twm/master/clancoliseum.sh -O -L &> /dev/null ;
+	curl $SERVER/clancoliseum.sh -O -L &> /dev/null ;
 	echo -e "\n Downloading 9/21 clandungeon.sh"
-	curl https://raw.githubusercontent.com/sharesourcecode/twm/master/clandungeon.sh -O -L &> /dev/null ;
+	curl $SERVER/clandungeon.sh -O -L &> /dev/null ;
 	echo -e "\n Downloading 10/21 clanfight.sh"
-	curl https://raw.githubusercontent.com/sharesourcecode/twm/master/clanfight.sh -O -L &> /dev/null ;
+	curl $SERVER/clanfight.sh -O -L &> /dev/null ;
 	echo -e "\n Downloading 11/21 clanid.sh"
-	curl https://raw.githubusercontent.com/sharesourcecode/twm/master/clanid.sh -O -L &> /dev/null ;
+	curl $SERVER/clanid.sh -O -L &> /dev/null ;
 	echo -e "\n Downloading 12/21 coliseum.sh"
-	curl https://raw.githubusercontent.com/sharesourcecode/twm/master/coliseum.sh -O -L &> /dev/null ;
+	curl $SERVER/coliseum.sh -O -L &> /dev/null ;
 	echo -e "\n Downloading 13/21 crono.sh"
-	curl https://raw.githubusercontent.com/sharesourcecode/twm/master/crono.sh -O -L &> /dev/null ;
+	curl $SERVER/crono.sh -O -L &> /dev/null ;
 	echo -e "\n Downloading 14/21 flagfight.sh"
-	curl https://raw.githubusercontent.com/sharesourcecode/twm/master/flagfight.sh -O -L &> /dev/null ;
+	curl $SERVER/flagfight.sh -O -L &> /dev/null ;
 	echo -e "\n Downloading 15/21 king.sh"
-	curl https://raw.githubusercontent.com/sharesourcecode/twm/master/king.sh -O -L &> /dev/null ;
+	curl $SERVER/king.sh -O -L &> /dev/null ;
 	echo -e "\n Downloading 16/21 loginlogoff.sh"
-	curl https://raw.githubusercontent.com/sharesourcecode/twm/master/loginlogoff.sh -O -L &> /dev/null ;
+	curl $SERVER/loginlogoff.sh -O -L &> /dev/null ;
 	echo -e "\n Downloading 17/21 play.sh"
-	curl https://raw.githubusercontent.com/sharesourcecode/twm/master/play.sh -O -L &> /dev/null ;
+	curl $SERVER/play.sh -O -L &> /dev/null ;
 	echo -e "\n Downloading 18/21 requeriments.sh"
-	curl https://raw.githubusercontent.com/sharesourcecode/twm/master/requeriments.sh -O -L &> /dev/null ;
+	curl $SERVER/requeriments.sh -O -L &> /dev/null ;
 	echo -e "\n Downloading 19/21 svproxy.sh"
-	curl https://raw.githubusercontent.com/sharesourcecode/twm/master/svproxy.sh -O -L &> /dev/null ;
+	curl $SERVER/svproxy.sh -O -L &> /dev/null ;
 	echo -e "\n Downloading 20/21 trade.sh"
-	curl https://raw.githubusercontent.com/sharesourcecode/twm/master/trade.sh -O -L &> /dev/null ;
+	curl $SERVER/trade.sh -O -L &> /dev/null ;
 	echo -e "\n Downloading 21/21 undying.sh"
-	curl https://raw.githubusercontent.com/sharesourcecode/twm/master/undying.sh -O -L &> /dev/null ;
+	curl $SERVER/undying.sh -O -L &> /dev/null ;
 	dos2unix *.sh &> /dev/null
 	chmod a+x -R $HOME/twm/
 	#cp -R twm/ storage/downloads/twm
