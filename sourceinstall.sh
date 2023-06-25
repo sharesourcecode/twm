8afdb119bb87e1cc0420f5fdd624bf8ceaf3cdff
Ueliton Alves Dos Santos <ueliton@disroot.org>
2023-06-25 16:47:11 -0300

    Add files via upload

--- sourceinstall.sh
+++ sourceinstall.sh
@@ -131,7 +131,7 @@ if awk -v remote="$remote_count" -v local="$local_count" 'BEGIN {if (remote == l
  slogan_func
  echo -e "${COLOR_CYAN}\n Wait for the scripts to download...☕👴${COLOR_RESET}"
  sync_func () {
-  SCRIPTS=(allies.sh altars.sh arena.sh campaign.sh career.sh cave.sh clancoliseum.sh clandungeon.sh clanfight.sh clanid.sh coliseum.sh crono.sh flagfight.sh king.sh loginlogoff.sh play.sh requeriments.sh run.sh svproxy.sh trade.sh undying.sh)
+  SCRIPTS=(allies.sh altars.sh arena.sh campaign.sh career.sh cave.sh clancoliseum.sh clandungeon.sh clanfight.sh clanid.sh coliseum.sh crono.sh flagfight.sh king.sh loginlogoff.sh play.sh requeriments.sh run.sh svproxy.sh trade.sh twm.sh undying.sh)
 #  SCRIPTS=(requeriments.sh svproxy.sh loginlogoff.sh crono.sh run.sh clanid.sh allies.sh altars.sh arena.sh campaign.sh career.sh cave.sh clancoliseum.sh clandungeon.sh clanfight.sh coliseum.sh flagfight.sh king.sh trade.sh undying.sh)
   NUM_SCRIPTS=${#SCRIPTS[@]}
   cd ~/twm
