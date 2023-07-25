d9efc4d3601ffa54b4a35247d3fbceb5ee58c07f
hugoviegas <hugoviegas3.0@gmail.com>
2023-07-25 05:11:45 +0100

    colection

--- check.sh
+++ check.sh
@@ -25,9 +25,20 @@ for (( i=0 ; i<11 ; i++ )) ; do
       w3m -cookie -o http_proxy=$PROXY -o accept_encoding=UTF-8 -debug -dump_source "${URL}${click}" -o user_agent="$(shuf -n1 $TMP/userAgent.txt)" >$TMP/SRC
     ) </dev/null &>/dev/null &
     time_exit 20
-    printf "${BLACK_YELLOW}Mission Completed (✔)${COLOR_RESET}\n"
+    printf "${BLACK_YELLOW}Relic collected (✔)${COLOR_RESET}\n"
   fi
+
+  (
+    w3m -cookie -o http_proxy=$PROXY -o accept_encoding=UTF-8 -debug -dump_source "${URL}/collector/reward/element/[?]r=[0-9]+" -o user_agent="$(shuf -n1 $TMP/userAgent.txt)" >$TMP/SRC
+  ) </dev/null &>/dev/null &
+ time_exit 20
+  if grep -o -E '/collector/reward/element/[?]r=[0-9]+' $TMP/SRC ; then
+    click=$(grep -o -E '/collector/reward/element/[?]r=[0-9]+' $TMP/SRC)
+    (
+      w3m -cookie -o http_proxy=$PROXY -o accept_encoding=UTF-8 -debug -dump_source "${URL}${click}" -o user_agent="$(shuf -n1 $TMP/userAgent.txt)" >$TMP/SRC
+    ) </dev/null &>/dev/null &
+    time_exit 20
+    printf "${BLACK_YELLOW}Collection collected (✔)${COLOR_RESET}\n"
 done
 }
 
-/relic/reward/5/?r=34009907
