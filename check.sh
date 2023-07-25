99d44163d84dba3616d621a78b8db6577909916a
hugoviegas <hugoviegas3.0@gmail.com>
2023-07-25 04:28:19 +0100

    ()

--- check.sh
+++ check.sh
@@ -4,23 +4,6 @@
 #/cave/attack/?r=42724370 #attack cave
 #/cave/runaway/?r=42724370 #runaway cave
 
-(
-  w3m -cookie -o -A 1 http_proxy=$PROXY -o accept_encoding=UTF-8 -dump "${url}${mana}{1,4}" -o user_agent="$(shuf -n1 $TMP/userAgent.txt)"|tee $TMP/info_file|sed -n '/[|]\ mp/,/\[arrow\]/p'|sed '1,1d;$d;6q' >> $TMP/msg_file
- ) </dev/null &>/dev/null &
- time_exit 17
-printf " ##### Titan #####\n" > $TMP/msg_file
-    #mana= "/images/icon/mana[.]png"
-    (
-        w3m -cookie -o -A 1 http_proxy=$PROXY -o accept_encoding=UTF-8 -dump "${url}/images/icon/mana[.]png{1,4}" -o user_agent="$(shuf -n1 $TMP/userAgent.txt)"|tee $TMP/info_file|sed -n '/[|]\ mp/,/\[arrow\]/p'|sed '1,1d;$d;6q' >> $TMP/msg_file
-    ) </dev/null &>/dev/null &
-    time_exit 17
-
-  #health= "/images/icon/health[.]png"
-    (
-        w3m -cookie -o -A 1 http_proxy=$PROXY -o accept_encoding=UTF-8 -dump "${url}/images/icon/health[.]png{1,4}" -o user_agent="$(shuf -n1 $TMP/userAgent.txt)"|tee $TMP/info_file|sed -n '/[|]\ mp/,/\[arrow\]/p'|sed '1,1d;$d;6q' >> $TMP/msg_file
-    ) </dev/null &>/dev/null &
-    time_exit 17
-
 check_cave () {
 (
   w3m -cookie -o http_proxy=$PROXY -o accept_encoding=UTF-8 -debug -dump_source "${URL}/cave/" -o user_agent="$(shuf -n1 $TMP/userAgent.txt)" >$TMP/SRC
--- twm.sh
+++ twm.sh
@@ -168,10 +168,12 @@ func_proxy
 messages_info () {
   printf " ##### Titan #####\n" > $TMP/msg_file
   
-    w3m -cookie -o http_proxy=$PROXY -o accept_encoding=UTF-8 -debug -dump_source "${URL}" -o user_agent="$(shuf -n1 $TMP/userAgent.txt)" > source.html
-    grep -o "${URL}/images/icon/2hit[.]png" source.html | grep -A 1 "${URL}/images/icon/2hit[.]png" source.html | tail -n 1
-    echo $(grep -A 1 "${URL}/images/icon/2hit[.]png" source.html | tail -n 1)
-
+  (w3m -cookie -o http_proxy=$PROXY -o accept_encoding=UTF-8 -debug -dump_source "${URL}" -o user_agent="$(shuf -n1 $TMP/userAgent.txt)" > source.html
+    grep -o "${URL}/images/icon/mana.png" source.html | grep -A 1 "${URL}/images/icon/mana.png" source.html | tail -n 1
+    echo $(grep -A 1 "${URL}/images/icon/mana.png" source.html | tail -n 1) >> $TMP/msg_file
+  ) </dev/null &>/dev/null &
+ time_exit 17
+ 
  printf " ##### mail #####\n" >> $TMP/msg_file
  (
   w3m -cookie -o http_proxy=$PROXY -o accept_encoding=UTF-8 -dump "${URL}/mail" -o user_agent="$(shuf -n1 $TMP/userAgent.txt)"|tee $TMP/info_file|sed -n '/[|]\ mp/,/\[arrow\]/p'|sed '1,1d;$d;6q' >> $TMP/msg_file
