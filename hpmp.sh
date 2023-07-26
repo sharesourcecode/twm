f4c28af05ba5fad176d91f7b0948daf64a35fe11
Ueliton Alves Dos Santos <ueliton@disroot.org>
2023-07-26 17:21:47 -0300

    HP and MP Analyzer

--- hpmp.sh
+++ hpmp.sh
@@ -7,7 +7,7 @@ hpmp () {
 
  #Fixed HP and MP
  FIXHP=$(grep -o -E '\(([0-9]+)\)' $TMP/SRC|sed 's/[()]//g')
- FIXMP=$(grep -m5 -o -E '[0-9]{1,5}'|sed -n '5p')
+ FIXMP=$(grep -o -E '[[:alpha:]]+: [0-9]{1,5}' $TMP/SRC|grep -m5 -o -E '[0-9]{1,5}'|sed -n '5p')
 
  #alt='hp'/> <span class='white'>19044</span> | <img src='/images/icon/mana.png' alt='mp'/> 1980</
  #Go to any page
@@ -15,7 +15,7 @@ hpmp () {
   w3m -cookie -o http_proxy=$PROXY -o accept_encoding=UTF-8 -debug -dump_source "${URL}/" -o user_agent="$(shuf -n1 $TMP/userAgent.txt)" >$TMP/SRC
  ) </dev/null &>/dev/null &
  time_exit 20
- STATUS=$(grep -o -E 'hp(.*)[0-9]{1,6}(.*)\|(.*)mp(.*)[0-9]{1,6}[<][/]span'|grep -o -E '[0-9]+ $TMP/SRC)
+ STATUS=$(grep -o -E 'hp(.*)[0-9]{1,6}(.*)\|(.*)mp(.*)[0-9]{1,6}[<][/]span' $TMP/SRC|grep -o -E '[0-9]+)
 
  #Variable HP and MP
  NOWHP=$(echo "$STATUS"|sed -n '1p')
