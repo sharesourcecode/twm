07e5de73fb9323fe7b051e2958bea6e9ce6fc72d
hugoviegas <hugoviegas3.0@gmail.com>
2023-07-26 03:18:02 +0100

    up

--- update.sh
+++ update.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
 BLACK_CYAN='\033[01;36m\033[01;07m'
+BLACK_GREEN='\033[00;32m\033[01;07m'
+COLOR_RESET='\033[00m'
 printf "${BLACK_CYAN} Upgrading...\n👉 Please wait...☕👴${COLOR_RESET}\n"
 cd ~/
 rm -rf $HOME/sourceinstall.sh
 curl https://raw.githubusercontent.com/sharesourcecode/twm/master/sourceinstall.sh -L -O
 sleep 3s
 chmod +x ./sourceinstall.sh
-printf "Updated... Starting"
+printf "${BLACK_GREENN}✅ Updated... Starting${COLOR_RESET}\n"
 ./sourceinstall.sh
