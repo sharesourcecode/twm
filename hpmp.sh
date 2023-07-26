6badd57598680cab25da486b3b74756b18e16309
Ueliton Alves Dos Santos <ueliton@disroot.org>
2023-07-26 17:10:15 -0300

    HP and MP Analyzer
    Example of a function that analyzes the current HP and MP, also in percentage.

---
+++ hpmp.sh
@@ -0,0 +1,29 @@
+hpmp () {
+ #Go to /train page
+ (
+  w3m -cookie -o http_proxy=$PROXY -o accept_encoding=UTF-8 -debug -dump_source "$URL/train" -o user_agent="$(shuf -n1 userAgent.txt)" >$TMP/SRC
+ ) &
+ time_exit 20
+
+ #Fixed HP and MP
+ FIXHP=$(grep -o -E '\(([0-9]+)\)' $TMP/SRC|sed 's/[()]//g')
+ FIXMP=$(grep -m5 -o -E '[0-9]{1,5}'|sed -n '5p')
+
+ #alt='hp'/> <span class='white'>19044</span> | <img src='/images/icon/mana.png' alt='mp'/> 1980</
+ #Go to any page
+ (
+  w3m -cookie -o http_proxy=$PROXY -o accept_encoding=UTF-8 -debug -dump_source "${URL}/" -o user_agent="$(shuf -n1 $TMP/userAgent.txt)" >$TMP/SRC
+ ) </dev/null &>/dev/null &
+ time_exit 20
+ STATUS=$(grep -o -E 'hp(.*)[0-9]{1,6}(.*)\|(.*)mp(.*)[0-9]{1,6}[<][/]span'|grep -o -E '[0-9]+ $TMP/SRC)
+
+ #Variable HP and MP
+ NOWHP=$(echo "$STATUS"|sed -n '1p')
+ NOWMP=$(echo "$STATUS"|sed -n '2p')
+
+ #Calculates percentage of HP and MP
+ HPPER=$(awk -v fixhp="$FIXHP" -v nowhp="$NOWHP" 'BEGIN { printf "%.0f", fixhp * nowhp / 100 }')
+ MPPER=$(awk -v fixmp="$FIXMP" -v nowmp="$NOWMP" 'BEGIN { printf "%.0f", fixmp * nowmp / 100 }')
+
+ printf "hp $NOWHP - ${HPPER}% | mp $NOWMP - ${MPPER}%\n"
+}
