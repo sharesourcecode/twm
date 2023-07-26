f2afb61c1c5e8e6465a1e89fc61b1231cff83213
hugoviegas <hugoviegas3.0@gmail.com>
2023-07-26 04:13:25 +0100

    up

--- update.sh
+++ update.sh
@@ -2,7 +2,7 @@
 BLACK_CYAN='\033[01;36m\033[01;07m'
 BLACK_GREEN='\033[00;32m\033[01;07m'
 COLOR_RESET='\033[00m'
-printf "Versions\n 1- Master\n 2- Beta1\n 3- Beta2"
+printf "Versions\n 1- Master\n 2- Beta1\n 3- Beta2\n"
 printf "Select the version: "
 read version
 case $version in
@@ -13,14 +13,13 @@ version="master"
 version="Beta-Teste"
 ;;
 (3)
-version="teste1"
+version="backup"
 esac
 echo $version
 printf "${BLACK_CYAN} Upgrading...👉 Please wait...☕👴${COLOR_RESET}\n"
 cd ~/
 rm -rf $HOME/sourceinstall.sh
-curl "https://raw.githubusercontent.com/sharesourcecode/twm/${version}/sourceinstall.sh" -L -O
-sleep 3s
-chmod +x ./sourceinstall.sh
+curl https://raw.githubusercontent.com/sharesourcecode/twm/${version}/sourceinstall.sh -s -L >$HOME/sourceinstall.sh
+chmod +x $HOME/sourceinstall.sh
 printf "${BLACK_GREENN}✅ Updated... Starting${COLOR_RESET}\n"
 ./sourceinstall.sh
