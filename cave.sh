c373bf2b6cf9a12eb6aa412335472731e16c69df
hugoviegas <hugoviegas3.0@gmail.com>
2023-07-27 01:20:30 +0100

    2.9

--- cave.sh
+++ cave.sh
@@ -107,22 +107,22 @@ echo "Cave..."
   w3m -cookie -o http_proxy=$PROXY -o accept_encoding=UTF-8 -debug -dump_source "${URL}/cave/" -o user_agent="$(shuf -n1 $TMP/userAgent.txt)" >$TMP/SRC
  ) </dev/null &>/dev/null &
  time_exit 20
- if grep -q -o -E '/cave/(gather|down|runaway)/[?]r[=][0-9]+' $TMP/SRC ; then #attack removed
-  #/'=\\\&apos
-  local CAVE=$(grep -o -E '/cave/(gather|down|runaway)/[?]r[=][0-9]+' $TMP/SRC)
-  local BREAK=$(( $(date +%s) + 60 ))
-  while [ -n "$CAVE" ] && [ $(date +%s) -lt "$BREAK" ] ; do
-   case $CAVE in
-    (*gather*|*down*|*runaway*) #attack removed
-    (
-     w3m -cookie -o http_proxy=$PROXY -o accept_encoding=UTF-8 -debug -dump_source "${URL}$CAVE" -o user_agent="$(shuf -n1 $TMP/userAgent.txt)" >$TMP/SRC
-    ) </dev/null &>/dev/null &
-    time_exit 20
-    echo "$CAVE"
+ if grep -q -o -E '/cave/(gather|down|runaway)/[?]r[=][0-9]+' $TMP/SRC ; then 
+    #/'=\\\&apos
     local CAVE=$(grep -o -E '/cave/(gather|down|runaway)/[?]r[=][0-9]+' $TMP/SRC)
-    ;;
-   esac
-  #done
+    local BREAK=$(( $(date +%s) + 60 ))
+    while [ -n "$CAVE" ] && [ $(date +%s) -lt "$BREAK" ] ; do
+      case $CAVE in
+        (*gather*|*down*|*runaway*) #attack removed
+          (
+           w3m -cookie -o http_proxy=$PROXY -o accept_encoding=UTF-8 -debug -dump_source "${URL}$CAVE" -o user_agent="$(shuf -n1 $TMP/userAgent.txt)" >$TMP/SRC
+          ) </dev/null &>/dev/null &
+          time_exit 20
+          echo "$CAVE"
+          local CAVE=$(grep -o -E '/cave/(gather|down|runaway)/[?]r[=][0-9]+' $TMP/SRC)
+        ;;
+      esac
+    done
   fi
   echo -e "${GREEN_BLACK}Cave (✔)${COLOR_RESET}\n"
 }
