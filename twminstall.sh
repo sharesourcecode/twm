7a6e6d2f7cdbc493ab68f819207ae9e94d6076ef
Ueliton Alves Dos Santos <ueliton@disroot.org>
2023-07-28 12:17:36 -0300

    shebang

--- twminstall.sh
+++ twminstall.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/bin/sh
 if [ ! -e "info.sh" ] ; then
 curl https://raw.githubusercontent.com/sharesourcecode/twm/beta1/info.sh -s -L -O >$HOME/info.sh
 chmod +x info.sh
@@ -104,7 +104,7 @@ script_slogan
 printf "${BLACK_CYAN}\n ⌛ Wait downloading scripts...${COLOR_RESET}\n"
 
 sync_func () {
-  SCRIPTS='allies.sh altars.sh arena.sh campaign.sh career.sh cave.sh check.sh clancoliseum.sh clandungeon.sh clanfight.sh clanid.sh coliseum.sh crono.sh flagfight.sh king.sh league.sh loginlogoff.sh play.sh requeriments.sh run.sh svproxy.sh trade.sh twm.sh undying.sh'
+  SCRIPTS="allies.sh altars.sh arena.sh campaign.sh career.sh cave.sh check.sh clancoliseum.sh clandungeon.sh clanfight.sh clanid.sh coliseum.sh crono.sh flagfight.sh king.sh league.sh loginlogoff.sh play.sh requeriments.sh run.sh svproxy.sh trade.sh twm.sh undying.sh"
   NUM_SCRIPTS=$(echo $SCRIPTS|wc -w)
   LEN=0
   for script in $SCRIPTS ; do
