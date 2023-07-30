d12f067a2ae700e952d66e73d85256307b8a1b27
Ueliton Alves Dos Santos <ueliton@disroot.org>
2023-07-30 16:46:19 -0300

    Add files via upload

--- twminstall.sh
+++ twminstall.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-if [ ! -e "info.sh" ] ; then
+if [ ! -e "info.sh" ]; then
  curl https://raw.githubusercontent.com/sharesourcecode/twm/beta1/info.sh -s -L >$HOME/info.sh
  chmod +x info.sh
  sleep 0.5s
@@ -18,7 +18,7 @@ else
 fi
 SERVER="https://raw.githubusercontent.com/sharesourcecode/twm/$version/"
 remote_count=$(curl ${SERVER}twminstall.sh -s -L|wc -c)
-if [ -e "twminstall.sh" ] ; then
+if [ -e "twminstall.sh" ]; then
  local_count=$(wc -c < "twminstall.sh")
 else
  local_count=1
@@ -28,75 +28,81 @@ cd ~/
 printf "${BLACK_CYAN} Installing TWM...\n⌛ Please wait...⌛${COLOR_RESET}\n"
 
 #termux
-if [ -d /data/data/com.termux/files/usr/share/doc ] ; then
+if [ -d /data/data/com.termux/files/usr/share/doc ]; then
  termux-wake-lock
  sed -u -i '/nameserver/d' $PREFIX/etc/resolv.conf &>/dev/null
  printf "nameserver 94.140.14.15\nnameserver 94.140.15.16" >$PREFIX/etc/resolv.conf
  LS="/data/data/com.termux/files/usr/share/doc"
- rm -rf ~/.termux/boot/play.sh 2> /dev/null
+ rm -rf ~/.termux/boot/play.sh 2>/dev/null
  mkdir -p ~/.termux/boot
- echo "IyEvZGF0YS9kYXRhL2NvbS50ZXJtdXgvZmlsZXMvdXNyL2Jpbi9zaApiYXNoICRIT01FL3R3bS90d20uc2ggLWJvb3QK"|base64 -d >~/.termux/boot/play.sh 2> /dev/null
- chmod +x ~/.termux/boot/play.sh 2> /dev/null
- if whereis w3m &>/dev/null ; then
+ echo "IyEvZGF0YS9kYXRhL2NvbS50ZXJtdXgvZmlsZXMvdXNyL2Jpbi9zaApiYXNoICRIT01FL3R3bS90d20uc2ggLWJvb3QK"|base64 -d >~/.termux/boot/play.sh 2>/dev/null
+ chmod +x ~/.termux/boot/play.sh 2>/dev/null
+ if whereis -b w3m </dev/null &>/dev/null; then
   :
  else
   pkg install w3m -y
  fi
- if whereis coreutils &>/dev/null ; then
+
+ if whereis -b coreutils </dev/null &>/dev/null; then
   :
  else
  pkg install coreutils ncurses-utils -y
  fi
- if [ -e "${LS}/termux-api" ] ; then
+
+ if [ -e "${LS}/termux-api" ]; then
   :
  else
   pkg install termux-api -y
  fi
- if [ -e "${LS}/procps" ] ; then
+
+ if [ -e "${LS}/procps" ]; then
   :
  else
   pkg install procps ncurses-utils -y
  fi
-fi #ls /data/...
+fi
 
 #/cygwin
-if uname|grep -q -i cygwin ; then
+if uname|grep -q -i "cygwin"; then
  LS="/usr/share/doc"
- if [ -e /bin/apt-cyg ] ; then
+ if [ -e /bin/apt-cyg ]; then
   :
  else
   #/cygwin repository
   curl -s -L -O "http://raw.githubusercontent.com/sharesourcecode/apt-cyg/master/apt-cyg" &>/dev/null
   install apt-cyg /bin
- fi #ls /bin
- if [ -e "${LS}/w3m" ] ; then
+ fi
+
+ if [ -e "${LS}/w3m" ]; then
   :
  else
   apt-cyg install w3m -y &>/dev/null
  fi
- if [ -e "${LS}/ncurses-term" ] ; then
+
+ if [ -e "${LS}/ncurses-term" ]; then
   :
  else
   apt-cyg install ncurses-term -y &>/dev/null
  fi
- if [ "${LS}/coreutils" ] ; then
+
+ if [ "${LS}/coreutils" ]; then
   :
  else
   apt-cyg install coreutils -y &>/dev/null
  fi
- if [ "${LS}/procps" ] ; then
+ if [ "${LS}/procps" ]; then
   :
  else
   apt-cyg install procps -y &>/dev/null
  fi
-fi #cygwin
+fi
 
 #linux
-if uname -o|grep -q -i GNU/Linux ; then
+if uname -o|grep -q -i "GNU/Linux"; then
  LS='/usr/share/doc'
  printf "Install the necessary packages for Alpine on Iphone(ISh), or android(UserLAnd):\n apk update\n apk add curl ; apk add w3m ; apk add coreutils ; apk add --no-cache tzdata\n\nInstall required packages for Linux or Windows WSL:\n sudo apt update\n sudo apt install curl coreutils ncurses-term procps w3m -y\n"
  sleep 5s
-fi #uname -o
+fi
 
 #starting...
 unset LS
@@ -112,11 +118,13 @@ sync_func () {
   LEN=$((LEN+1))
   printf "Checking $LEN/$NUM_SCRIPTS $script\n"
   remote_count=$(curl ${SERVER}$script -s -L|wc -c)
+
   if [ -e ~/twm/$script ]; then
    local_count=$(wc -c < "$script")
   else
    local_count=1
   fi
+
   if [ -e ~/twm/$script ] && [ "$remote_count" -eq "$local_count" ]; then
    printf "✅ ${BLACK_CYAN}Updated $script${COLOR_RESET}\n"
   elif [ -e ~/twm/$script ] && [ "$remote_count" -ne "$local_count" ]; then
@@ -140,15 +148,17 @@ sync_func_other () {
  curl ${SERVER}twm.sh -s -L|sed -n '1,33p' >twm.sh
  NUM_SCRIPTS=$(echo $SCRIPTS|wc -w)
  LEN=0
- for script in $SCRIPTS ; do
- LEN=$((LEN+1))
- printf "Checking $LEN/$NUM_SCRIPTS $script\n"
- printf "🔁 ${BLACK_GREEN}Updating $script${COLOR_RESET}\n"
- curl ${SERVER}$script -s -L >>twm.sh
- printf "\n#\n" >>twm.sh
- sleep 0.1s
+
+ for script in $SCRIPTS; do
+  LEN=$((LEN+1))
+  printf "Checking $LEN/$NUM_SCRIPTS $script\n"
+  printf "🔁 ${BLACK_GREEN}Updating $script${COLOR_RESET}\n"
+  curl ${SERVER}$script -s -L >>twm.sh
+  printf "\n#\n" >>twm.sh
+  sleep 0.1s
  done
  curl ${SERVER}twm.sh -s -L|sed -n '35,87p' >>twm.sh
+
  case $(uname -o) in
   (Android)
    :
@@ -172,24 +182,24 @@ esac
 
 script_slogan
 printf "✅ ${BLACK_CYAN}Updated scripts!${COLOR_RESET}\n To execute run command: ${GOLD_BLACK}./twm/play.sh${COLOR_RESET}\n       For coliseum run: ${GOLD_BLACK}./twm/play.sh -cl${COLOR_RESET}\n           For cave run: ${GOLD_BLACK}./twm/play.sh -cv${COLOR_RESET}\n"
-pidf=$(ps ax -o pid=,args=|grep "sh.*twm/play.sh"|grep -v 'grep'|head -n1|grep -o -E '([0-9]{3,5})')
-until [ -z $pidf ] ; do
- kill -9 $pidf 2> /dev/null
- pidf=$(ps ax -o pid=,args=|grep "sh.*twm/play.sh"|grep -v 'grep'|head -n1|grep -o -E '([0-9]{3,5})')
+tipidf=$(ps ax -o pid=,args=|grep "sh.*twm/play.sh"|grep -v 'grep'|head -n 1|grep -o -E '([0-9]{3,5})')
+until [ -z $tipidf ]; do
+ kill -9 $tipidf 2>/dev/null
+ tipidf=$(ps ax -o pid=,args=|grep "sh.*twm/play.sh"|grep -v 'grep'|head -n 1|grep -o -E '([0-9]{3,5})')
  sleep 1s
 done
-pidf=$(ps ax -o pid=,args=|grep "sh.*twm/twm.sh"|grep -v 'grep'|head -n1|grep -o -E '([0-9]{3,5})')
-until [ -z $pidf ] ; do
- kill -9 $pidf 2> /dev/null
- pidf=$(ps ax -o pid=,args=|grep "sh.*twm/twm.sh"|grep -v 'grep'|head -n1|grep -o -E '([0-9]{3,5})')
+tipidf=$(ps ax -o pid=,args=|grep "sh.*twm/twm.sh"|grep -v 'grep'|head -n 1|grep -o -E '([0-9]{3,5})')
+until [ -z $tipidf ]; do
+ kill -9 $tipidf 2>/dev/null
+ tipidf=$(ps ax -o pid=,args=|grep "sh.*twm/twm.sh"|grep -v 'grep'|head -n 1|grep -o -E '([0-9]{3,5})')
  sleep 1s
 done
-if [ -f ~/twm/runmode_file ] ; then
- if awk '{if ($0 == "-cv") print $0}' ~/twm/runmode_file ; then
+if [ -f ~/twm/runmode_file ]; then
+ if awk '{if ($0 == "-cv") print $0}' ~/twm/runmode_file; then
   printf "${BLACK_GREEN}Automatically restarting in 5s after update...${COLOR_RESET}\n"
   sleep 5s
   ~/twm/play.sh -cl
- elif awk '{if ($0 == "-cv") print $0}' ~/twm/runmode_file ; then
+ elif awk '{if ($0 == "-cv") print $0}' ~/twm/runmode_file; then
   printf "${BLACK_GREEN}Automatically restarting in 5s after update...${COLOR_RESET}\n"
   sleep 5s
   ~/twm/play.sh -cv
