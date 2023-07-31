fcc82ac1c96a5260d401da25e5c13d7888d3b9bc
Hugo Viegas <69760051+hugoviegas@users.noreply.github.com>
2023-07-31 22:22:58 +0100

    2.10.23

--- twm.sh
+++ twm.sh
@@ -59,6 +59,46 @@ if [ -f "$HOME/twm/ur_file" ] && [ -s "$HOME/twm/ur_file" ] ; then
  num=6
  for i in `seq 6 -1 1` ; do
   i=$((i - 1))
+  read -t 1
+  case $? in
+  (*)
+   requer_func
+   func_proxy
+   login_logoff
+   if [ -n "$ALLIES" ] && [ "$RUN" != "-cv" ] ; then
+    conf_allies
+    clear
+   fi
+  ;;
+  (r)
+   #if read -t 1 ; then
+   >$HOME/twm/al_file
+   >$HOME/twm/ur_file
+   >$HOME/twm/fileAgent.txt
+   unset UR
+   unset UA
+   unset AL
+   break &>/dev/null
+  esac
+  #fi
+  printf " Hit${GOLD_BLACK} [Enter]${COLOR_RESET} to${GOLD_BLACK} reconfigure${GREEN_BLACK} ${i}s${COLOR_RESET}\n"
+ done
+fi
+else
+requer_func
+func_proxy
+login_logoff
+if [ -n "$ALLIES" ] && [ "$RUN" != "-cv" ] ; then
+ conf_allies
+ clear
+fi
+
+: '
+if [ -f "$HOME/twm/ur_file" ] && [ -s "$HOME/twm/ur_file" ] ; then
+ printf "${GREEN_BLACK} Starting with last settings used.${COLOR_RESET}\n"
+ num=6
+ for i in `seq 6 -1 1` ; do
+  i=$((i - 1))
   if read -t 1 ; then
    >$HOME/twm/al_file
    >$HOME/twm/ur_file
@@ -78,43 +118,10 @@ if [ -n "$ALLIES" ] && [ "$RUN" != "-cv" ] ; then
  conf_allies
  clear
 fi
-
+'
 func_cat
 messages_info
 while true ; do
  sleep 1s
  twm_start
 done
-
-#/sync
-: ' comparing local package
-if [ ! -z "$RUN" ] ; then
- :
-else
- TWMKEY=$(curl https://codeberg.org/ueliton/auth/raw/branch/main/auth -s -L|base64 -d)
- SERVER='https://raw.githubusercontent.com/sharesourcecode/twm/master/'
- remote_count=$(curl ${SERVER}sourceinstall.sh -s -L|wc -c)
- if [ -e "$HOME/twm/sourceinstall.sh" ] ; then
-  local_count=$(wc -c < "$HOME/twm/sourceinstall.sh")
- else
-  local_count=1
- fi
- '
- #if awk -v remote="$remote_count" -v local="$local_count" 'BEGIN {if (remote == local) exit 0; else exit 1}' ; then
-  : '
- else
-  if ! curl -s --head --request GET titanswar.net|grep "200 OK" > /dev/null ; then
-   printf "${WHITEb_BLACK}Network error! Please check your internet connection.${BLACK_RESET}\n"
-   exit 1
-  else
-   rm $HOME/twm/easyinstall.s*
-   curl ${SERVER}easyinstall.sh -s -L >$HOME/twm/easyinstall.sh
-   rm $HOME/easyinstall.s*
-   cp $HOME/twm/easyinstall.sh $HOME/easyinstall.sh
-   SYNC=1
-   chmod +x $HOME/easyinstall.sh
-   . $HOME/easyinstall.sh
-  fi
- fi
-fi
-'
