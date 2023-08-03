cb60cff62621be35121dbc7e5e669869b12e5605
Hugo Viegas <69760051+hugoviegas@users.noreply.github.com>
2023-08-03 01:41:27 +0100

    2.11.01

--- hpmp.sh
+++ hpmp.sh
@@ -8,20 +8,22 @@ hpmp () {
   )  </dev/null &>/dev/null &
   time_exit 20
  fi
+ echo "passei"
  #/Fixed HP and MP.
  #/Needs to run -fix at least once before
  FIXHP=$(grep -o -E '\(([0-9]+)\)' $TMP/TRAIN|sed 's/[()]//g')
  FIXMP=$(grep -m5 -o -E '[0-9]{1,5}' $TMP/TRAIN|sed -n '5p')
 
+printf "max hp: {$FIXHP} max mp {$FIXMP}"
 
  #/$STATUS can be obtained from any SRC file
  #/alt='hp'/> <span class='white'>19044</span> | <img src='/images/icon/mana.png' alt='mp'/> 1980</
  local STATUS=$(grep -o -E 'hp(.*)[0-9]{1,6}(.*)\|(.*)mp(.*)[0-9]{1,6}[<][/]span'|grep -o -E '[0-9]+ $TMP/SRC')
-
+printf "status {$STATUS}"
  #/Variable HP and MP
  NOWHP=$(echo "$STATUS"|sed -n '1p')
  NOWMP=$(echo "$STATUS"|sed -n '2p')
-
+printf "Actual Hp {$NOWHP} actual mp {$NOWMP}"
  #/Calculates percentage of HP and MP.
  #/Needs to run -fix at least once before
  HPPER=$(awk -v fixhp="$FIXHP" -v nowhp="$NOWHP" 'BEGIN { printf "%.0f", fixhp * nowhp / 100 }')
