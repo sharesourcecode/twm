8ed9bcdd36d6e0cf5615d623390cedb632cb37cf
Hugo Viegas <69760051+hugoviegas@users.noreply.github.com>
2023-08-03 01:46:27 +0100

    2.11.01

--- hpmp.sh
+++ hpmp.sh
@@ -18,7 +18,7 @@ printf "max hp: {$FIXHP} max mp {$FIXMP}"
 
  #/$STATUS can be obtained from any SRC file
  #/alt='hp'/> <span class='white'>19044</span> | <img src='/images/icon/mana.png' alt='mp'/> 1980</
- local STATUS=$(grep -o -E 'hp(.*)[0-9]{1,6}(.*)\|(.*)mp(.*)[0-9]{1,6}[<][/]span'|grep -o -E '[0-9]+ $TMP/SRC')
+ local STATUS=$(grep -o -E 'hp(.*)[0-9]{1,6}(.*)\|(.*)mp(.*)[0-9]{1,6}[<][/]span'|grep -o -E '[0-9]+' $TMP/SRC)
 printf "status {$STATUS}"
  #/Variable HP and MP
  NOWHP=$(echo "$STATUS"|sed -n '1p')
