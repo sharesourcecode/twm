beece51279f6566f1cb741f66e42de68d4890ee5
Hugo Viegas <69760051+hugoviegas@users.noreply.github.com>
2023-08-03 03:04:37 +0100

    2.11.02

--- hpmp.sh
+++ hpmp.sh
@@ -13,8 +13,8 @@ hpmp () {
  #/Needs to run -fix at least once before
  FIXHP=$(grep -o -E '\(([0-9]+)\)' $TMP/TRAIN|sed 's/[()]//g')
 
- FIXMP=$(grep -o -E -A1 ': ' $TMP/TRAIN|sed -n 's/[0-9]\p')
-  printf "\nTeste : $FIXMP"
+ FIXMP=$(grep -o -E -A1 '\: \' $TMP/TRAIN) #|sed -n '[0-9]/p')
+  printf "\nTeste : $FIXMP \n"
   : '
   FIXMP=$(grep -o -E '[0-9]{1,5}' $TMP/TRAIN|sed -n '2p')
   printf "\nTeste : $FIXMP"
@@ -36,18 +36,18 @@ hpmp () {
   printf "\nTeste : $FIXMP"
   FIXMP=$(grep -o -E '[0-9]{1,5}' $TMP/TRAIN|sed -n '11p')
   printf "\nTeste : $FIXMP"
-'
+    '
 
-printf "max hp: {$FIXHP} max mp {$FIXMP}"
+ printf "max hp: {$FIXHP} max mp {$FIXMP}"
 
  #/$STATUS can be obtained from any SRC file
  #/alt='hp'/> <span class='white'>19044</span> | <img src='/images/icon/mana.png' alt='mp'/> 1980</
  local STATUS=$(grep -o -E 'hp(.*)[0-9]{1,6}(.*)\|(.*)mp(.*)[0-9]{1,6}[<][/]span'|grep -o -E '[0-9]+' $TMP/SRC)
-#printf "status {$STATUS}"
+ #printf "status {$STATUS}"
  #/Variable HP and MP
  NOWHP=$(echo "$STATUS"|sed -n '1p')
  NOWMP=$(echo "$STATUS"|sed -n '2p')
-printf "Actual Hp {$NOWHP} actual mp {$NOWMP}"
+ printf "Actual Hp {$NOWHP} actual mp {$NOWMP}"
  #/Calculates percentage of HP and MP.
  #/Needs to run -fix at least once before
  HPPER=$(awk -v fixhp="$FIXHP" -v nowhp="$NOWHP" 'BEGIN { printf "%.0f", fixhp * nowhp / 100 }')
