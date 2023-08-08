3c24aa1597e2c45e64b767e6bda9ed5ce91a7b49
Ueliton Alves Dos Santos <ueliton@disroot.org>
2023-08-08 20:08:46 -0300

    Add files via upload

--- arena.sh
+++ arena.sh
@@ -113,12 +113,12 @@ arena_duel () {
  local BREAK=$(( $(date +%s) + 60 ))
  until grep -q -o 'lab/wizard' $TMP/SRC || [ $(date +%s) -gt "$BREAK" ] ; do
   #icon=$(grep -q -o -A 1 "/images/icon/race/0.png" $TMP/SRC|sed -n '1p')
-  local ACCESS=$(grep -o -E '(/arena/attack/1/[^A-Za-z0-9]r[^A-Za-z0-9][0-9]+)' $TMP/SRC|sed -n '1p') #/arena/attack/1/1234567*/
+  local ACCESS=$(grep -o -E '(/arena/attack/1/[?]r[=][0-9]+)' $TMP/SRC|sed -n '1p') #/arena/attack/1/1234567*/
   (
    w3m -cookie -o http_proxy=$PROXY -o accept_encoding=UTF-8 -debug -dump_source "${URL}${ACCESS}" -o user_agent="$(shuf -n1 $TMP/userAgent.txt)" >$TMP/SRC
   ) </dev/null &>/dev/null &
   time_exit 17
-  echo " ⚔ $ACCESS"
+  echo " ⚔ ${ACCESS}"
   sleep 1s
  done
  arena_deleteEnd
