7d766ee4dc5d5efee2de23ffaba5e72c52426de2
Ueliton Alves Dos Santos <ueliton@disroot.org>
2024-11-21 14:24:18 -0300

    Significant Changes to `play.sh`
    The `play.sh` script has undergone a complete overhaul compared to its predecessor. The most notable changes include:
    
    * **Infinite Loop Process Management:** The `play.sh` script now operates within an infinite loop, monitoring and restarting the main `twm.sh` script when necessary. This approach ensures that the macro continues to run even if the main script encounters errors or is interrupted.
    
    * **Argument-Based Execution Simplification:** The execution logic based on the `-cl`, `-cv`, and `-boot` arguments has been optimized and integrated directly into the `run_mode` function within the loop.
    
    * **Redundant Code Removal:** The script structure has been simplified, removing redundant and unnecessary code present. * **More Efficient Function Calls:** The way the `run_mode` and `twm.sh` functions are called has been optimized, resulting in cleaner, more efficient code.
    
    In short, the new `play.sh` is more robust, efficient, and easier to understand, ensuring continuous macro execution and simplifying its use.

--- play.sh
+++ play.sh
@@ -3,29 +3,48 @@
 # Copyright (c) 2019-2024 Ueliton Alves Dos Santos
 # Licensed under the Creative Commons Attribution-NonCommercial-ShareAlike 4.0 International License
 
-(
- RUN=$1
- twm_dir="twm-master"
- echo "$RUN" >$HOME/$twm_dir/runmode_file
- while true; do
+RUN=$1
+twm_dir="twm-master"
+echo "$RUN" >$HOME/$twm_dir/runmode_file
+
+run_mode() {
+
+  case "$RUN" in
+    "-cl")
+      chmod +x $HOME/$twm_dir/twm.sh
+      $HOME/$twm_dir/twm.sh #&
+      wait
+    ;;
+    "-cv")
+      chmod +x $HOME/$twm_dir/twm.sh
+      $HOME/$twm_dir/twm.sh -cv #&
+      wait
+    ;;
+    "-boot")
+      echo '-boot' >$HOME/$twm_dir/runmode_file ; chmod +x $HOME/$twm_dir/twm.sh
+      $HOME/$twm_dir/twm.sh -boot #&
+      wait
+    ;;
+    *)
+      echo '-boot' >$HOME/$twm_dir/runmode_file
+      chmod +x $HOME/$twm_dir/twm.sh
+      $HOME/$twm_dir/twm.sh -boot #&
+      wait
+    ;;
+  esac
+
+}
+
+while true; do
   pidf=$(jobs -l | awk '/sh.*$twm_dir\/twm\.sh/ { if (NR == 1) print $2 }')
-  until [ -z "${pidf}" ]; do
-   kill -9 ${pidf} 2>/dev/null
-   pidf=$(jobs -l | awk '/sh.*$twm_dir\/twm\.sh/ { if (NR == 1) print $2 }')
-   sleep 1s
-  done
-  run_mode () {
-   if echo "$RUN"|grep -q -E '[-]cl'; then
-    chmod +x $HOME/$twm_dir/twm.sh ; $HOME/$twm_dir/twm.sh
-   elif echo "$RUN"|grep -q -E '[-]cv'; then
-    chmod +x $HOME/$twm_dir/twm.sh ; $HOME/$twm_dir/twm.sh -cv
-   elif echo "$RUN"|grep -q -E '[-]boot'; then
-    echo '-boot' >$HOME/$twm_dir/runmode_file ; chmod +x $HOME/$twm_dir/twm.sh ; $HOME/$twm_dir/twm.sh -boot
-   else
-    echo '-boot' >$HOME/$twm_dir/runmode_file ; chmod +x $HOME/$twm_dir/twm.sh ; $HOME/$twm_dir/twm.sh -boot
-   fi
-  }
+
+  if [ -z "${pidf}" ]; then
+    sync
+    kill -9 ${pidf} > /dev/null 2>&1
+    pidf=$(jobs -l | awk '/sh.*$twm_dir\/twm\.sh/ { if (NR == 1) print $2 }')
+    sleep 1s
+  fi
+
   run_mode
   sleep 0.1s
- done
-)
+done
