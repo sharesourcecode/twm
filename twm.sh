84c4a3cea41f092b58e94d99701324cb6e7812ab
Ueliton Alves Dos Santos <ueliton@disroot.org>
2025-02-08 22:45:39 -0300

    Add files via upload

--- twm.sh
+++ twm.sh
@@ -83,32 +83,30 @@ files="$HOME/$twm_dir/lang_file $HOME/$twm_dir/al_file $HOME/$twm_dir/ur_file $H
 for file in $files; do
 
  if test -e "$file"; then
+   status=0
    continue
 
    if test -s "$file"; then
+     status=0
      continue
    else
-     exit 1
-#     break
+     status=1
    fi
 
  else
-   exit 1
-#   break
- fi
-
- status=$?
+   status=1
+ fi || status=$?
 
 done
 
-if [ -z "$status" ]; then
+if [ "$status" -eq 0 ]; then
   printf "${GREEN_BLACK} $(G_T "Starting with last settings used").${COLOR_RESET}\n"
   printf "$(G_T "Hit")${GOLD_BLACK} [Enter]${COLOR_RESET} $(G_T "to reconfigure, or wait")${GREEN_BLACK} 5$(G_T "s")${COLOR_RESET}\n"
   ( sleep 5s && pkill -SIGTERM -s 0 dd ) </dev/null &>/dev/null &
   input=$(dd bs=1 count=1 2>/dev/null) || input=3
 fi
 
-if [ -z "$input" ] || [ ! -z "$status" ]; then
+if [ -z "$input" ] || [ "$status" -eq 1 ]; then
   >$HOME/$twm_dir/lang_file
   >$HOME/$twm_dir/al_file
   >$HOME/$twm_dir/ur_file
